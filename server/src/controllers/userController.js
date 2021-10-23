cc9045c5f149c14ad2b2d1fd0912835de67b1639
abdallah1996adam <abdallahaadam@protonmail.com>
2021-10-23 22:43:24 +0200

    :zap:

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -1,10 +1,19 @@
+
 const express = require('express')
 const crypto = require('crypto')
-const {connect} = require('stream-chat')
+const {connect} = require('getstream')
+const bcrypt = require('bcrypt')
+const streamChat = require('stream-chat')
 
 exports.signup = async (request, response)=>{
     try {
+        const userId = crypto.randomBytes(16).toString('hex')
+
+
         console.log(request.body);
+
+
+
     } catch (error) {
         console.log(error);
         return response.status(500).json({Error: error.message})
